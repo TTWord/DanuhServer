af8b5eb4a9e4b86170e7f3455d671f609122980c
reality023 <reality023@naver.com>
2023-02-25 12:14:06 +0900

    feat: CREATE SQL 변경

--- db/create.sql
+++ db/create.sql
@@ -1,4 +1,38 @@
+drop table user;
+drop table book;
+drop table word;
+drop table certification;
+
+create table user(
+    id INT PRIMARY KEY auto_increment,
+    username VARCHAR(100),
+    password VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
 create table book(
-	  id INT PRIMARY KEY auto_increment,
-    name VARCHAR(100)
+    id INT PRIMARY KEY auto_increment,
+    user_id INT,
+    name VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
+create table word(
+    id INT PRIMARY KEY auto_increment,
+    book_id INT,
+    word VARCHAR(100),
+    mean VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+
+create table certification(
+    id INT PRIMARY KEY auto_increment,
+    type VARCHAR(100),
+    cert_key VARCHAR(100),
+    cert_code VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
