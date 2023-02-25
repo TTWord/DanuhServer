9ea2de340c10adff92a270f93b0676c581754479
reality023 <reality023@naver.com>
2023-02-25 11:58:28 +0900

    feat: DB 연결 예시 코드 작성

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-import flask
 from api.index import main_route
+from flask_cors import CORS
+from flask import Flask
+from config import config
+      
+app = Flask(__name__)
 
-app = flask.Flask(__name__)
+# Connect Config to app.config
+app.config["SECRET_KEY"] = config["SECRET_KEY"]
+app.config["JSON_AS_ASCII"] = False
 
+CORS(app)
 app.register_blueprint(main_route)
 
-if __name__ == "__main__":  
-    app.run(host='0.0.0.0', port='5001', debug=True)
+if __name__ == "__main__": 
+    app.run(host="127.0.0.1", port=5001, debug=True)
---
+++ model/book.py
@@ -0,0 +1,28 @@
+from config import Database
+
+class Book:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return f"Book({self.id}, {self.name})"
+    
+    @staticmethod
+    def get_books():
+        db = Database()
+        
+        db.connect()
+        
+        books = []
+        with db.connection.cursor() as cursor:
+            sql = 'SELECT * FROM book'
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            
+            for row in result:
+                books.append(Book(id=row[0], name=row[1]))
+        
+        db.disconnect()
+        
+        return [book.__dict__ for book in books]
