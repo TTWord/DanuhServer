ac50c74a2cc889f6c85e1b27b7a69b490996035a
reality023 <reality023@naver.com>
2023-03-07 23:39:20 +0900

    feat: 단어장 조회, 단어장 추가 작업

--- repository/book_repository.py
+++ repository/book_repository.py
@@ -1,9 +1,7 @@
 from db.connect import Connect
 from model.book_model import BookModel
-from util.decorator.exporter import RepositoryExporter as exporter
 
 class BookRepository(Connect):
-    @exporter
     def find_all(self) -> list:
         books = []
         
@@ -14,20 +12,18 @@ class BookRepository(Connect):
         for row in result:
             books.append(BookModel(id=row['id'], name=row['name']))
         
-        return books
+        return books.__dict__
     
-    @exporter
     def find_all_by_user_id(self, user_id: int):
         books = []
         
-        result = self.select_all('SELECT * FROM book WHERE user_id = {user_id}')
+        result = self.select_all(f'SELECT * FROM book WHERE user_id = {user_id}')
         
         for row in result:
             books.append(BookModel(id=row['id'], name=row['name']))
         
-        return books
+        return books.__dict__
     
-    @exporter
     def find_one_by_id(self, id: int) -> dict:
         book = None
 
@@ -37,29 +33,38 @@ class BookRepository(Connect):
             
         book = BookModel(id=result['id'], name=result['name'])
             
-        return book
+        return book.__dict__
     
-    @exporter
-    def add(self, name: str) -> dict:
-        sql = f"INSERT INTO book (name) VALUES ('{name}')"
+    def find_one_by_name_and_user_id(self, name: str, user_id: int) -> dict:
+        book = None
+        
+        sql = f'SELECT * FROM book WHERE name = "{name}" AND user_id = {user_id}'
         self.cursor.execute(sql)
-        self.connection.commit()
+        result = self.cursor.fetchone()
         
-        book = BookModel(id=self.cursor.lastrowid, name=name)
+        if result is not None:
+            book = BookModel(id=result['id'], name=result['name'])
         
-        return book
+        return book.__dict__
     
-    @exporter
+    def add(self, user_id: int, name: str) -> dict:
+        sql = f"INSERT INTO book (user_id, name) VALUES ({user_id}, '{name}')"
+        self.cursor.execute(sql)
+        self.connect.commit()
+        
+        book = BookModel(id=self.cursor.lastrowid, user_id=user_id, name=name)
+        
+        return book.__dict__
+    
     def update(self, id: int, name: str) -> dict:
         sql = f"UPDATE book SET name = '{name}' WHERE id = {id}"
         self.cursor.execute(sql)
-        self.connection.commit()
+        self.connect.commit()
         
         book = BookModel(id=id, name=name)
         
-        return book
+        return book.__dict__
         
-    @exporter
     def delete(self, id: int):
         sql = f"DELETE FROM book WHERE id = {id}"
         self.cursor.execute(sql)
