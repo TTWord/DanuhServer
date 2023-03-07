fe4341f9d423c8f1ebf06746e00b6a2dcf687873
DESKTOP-0J10CUH\kimju <djsk721@naver.com>
2023-03-07 19:48:31 +0900

    feat: 번역 모델 적용 완료(임시)

--- api/book.py
+++ api/book.py
@@ -1,10 +1,13 @@
 from service.book_service import BookService
-from flask_restx import Namespace, Resource, Api
-from flask import request
+from flask_restx import Namespace, Resource, Api, reqparse, fields
 from util.decorator.authorization import Authorization
 
 
 api = Namespace('book', description='단어장 API')
+
+input_text = api.model('예측', {
+    'text': fields.String(required=True, description='예측 값 입력', example='You can tell a stranger that this is shiitake mushrooms.')
+})
 
 # getBookParser = api.parser()
 # getBookParser.add_argument('name', type=str, help='단어장 이름', location='args')
@@ -36,5 +39,23 @@ class BookById(Resource):
     def delete(self, id):
         return "delete_book"
     
+
+
+# TODO : Refactoring 필요
+import requests
+import json
+from flask import request
+from config import config
+@api.route('/predict')
+class Booktest(Resource):
+    @api.expect(input_text, validate=True)
+    def post(self):
+        url = f"{config['AI_IP']}"
+        input_data = request.get_json()
+        response = requests.post(url, json=input_data)
+        result = json.loads(response.content)
+
+        # Print the result
+        return result
     
     
