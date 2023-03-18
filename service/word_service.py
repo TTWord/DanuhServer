0c3efe80f7922046d4f50d34a8977953101d5aa2
DESKTOP-0J10CUH\kimju <djsk721@naver.com>
2023-03-19 00:09:31 +0900

    chore: word delete 수정

--- service/word_service.py
+++ service/word_service.py
@@ -78,5 +78,6 @@ class WordService:
                 if not word:
                     return make_response({'message': str(e)}, 404)
                 word = WordRepository(db).delete(id=id)
+                return make_response({'message': 'Succesfully Deleted'}, 200)
         except Exception as e:
             return make_response({'message': str(e)}, 404)
