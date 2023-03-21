efdcd30734ce8e0402b78157d7bcb1bc23db3844
DESKTOP-0J10CUH\kimju <djsk721@naver.com>
2023-03-21 20:46:51 +0900

    refector: 권한 분리 및 리팩토링

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from api.book import api as book_ns
 from api.word import api as word_ns
 from api.user import api as user_ns
+from api.auth import api as auth_ns
 
 
 authorizations = {
@@ -28,3 +29,4 @@ api = Api(
 api.add_namespace(book_ns)
 api.add_namespace(word_ns)
 api.add_namespace(user_ns)
+api.add_namespace(auth_ns)
---
+++ api/auth.py
@@ -0,0 +1,41 @@
+from flask import request
+from flask_restx import Namespace, Resource, Api, reqparse, fields
+from service.auth_service import AuthService
+
+
+api = Namespace('auth', description='관리 API')
+
+user_sign_in = api.model('회원 로그인', {
+    'username': fields.String(required=True, description='아이디', example='kimjunghyun696@google.com'),
+    'password': fields.String(required=True, description='비밀번호', example='13131313'),
+})
+
+email_content = api.model('이메일 인증', {
+    'to_email': fields.String(required=True, description='수신자 이메일 주소', example= 'djsk721@naver.com')
+})
+
+
+@api.route('/signin')
+class UserSignIn(Resource):
+    @api.expect(user_sign_in, validate=True)
+    @api.response(200, 'Success')
+    @api.response(400, 'Bad request')
+    def post(self):
+        """
+        로그인
+        """
+        input_data = request.get_json()
+        return AuthService.signin_service(input_data)
+        
+
+@api.route('/sendmail')
+class SendMail(Resource):
+    @api.expect(email_content, validate=True)
+    @api.response(200, 'Success')
+    @api.response(400, 'Bad request')
+    def post(self):
+        """
+        인증 메일 전송
+        """
+        input_data = request.get_json()
+        return AuthService.send_mail(input_data)
